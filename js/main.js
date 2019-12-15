944a3d9e699e070c7e948bfc32145ab95ddbfa89
Anton <aeprotsyuk@gmai.com>
2019-12-15 20:58:49 +0300

    feature: add project section, add slider in project section

--- js/main.js
+++ js/main.js
@@ -8,14 +8,9 @@ $(document).ready(function () {
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
-});
 
-/* button */
+  /* button top */
 
-$(document).ready(function () {
-  /**
-   * При прокрутке страницы, показываем или срываем кнопку
-   */
   $(window).scroll(function () {
     // Если отступ сверху больше 50px то показываем кнопку "Наверх"
     if ($(this).scrollTop() > 100) {
@@ -32,4 +27,28 @@ $(document).ready(function () {
     return false;
   });
 
+  /* slider */
+
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 10 + bullets.width() + 10);
+  bullets.css('left', prev.width() + 10)
 });
+
+/* button */
+
+
